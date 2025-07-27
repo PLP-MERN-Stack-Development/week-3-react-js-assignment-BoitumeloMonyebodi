95a594cb4a64b80024d35aafad9e45450f291167
Boitumelo Monyebodi <linkymonyebodi1@gmail.com>
2025-07-27 23:53:40 +0200

    Delete FOODRECEIPE directory

--- FOODRECEIPE/backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signupUser, loginUser, getUseById } = require("../controller/user");
-
-router.post("/signup", registerUser);
-router.post("/login", loginUser);
-router.get("/:id", getUserById);
-
-module.exports = router;
